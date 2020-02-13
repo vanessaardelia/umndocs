d809132cabfe9a312b75e74aab0874f02378c8b7
Goldenbabyy <ventzicalovenia@gmail.com>
2020-02-13 13:42:19 +0700

    buat nesa

--- controllers/MainController.php
+++ controllers/MainController.php
@@ -12,19 +12,19 @@ class MainController extends \yii\web\Controller
                         JOIN M_Document ON M_Revisi.IdDoc = M_Document.IdDoc";
         $documents = Yii::$app->db->createCommand($query);
         $emailUser = Yii::$app->getRequest()->getCookies()->getValue('emailUser');
-        $result = $documents->query();
+        $result = $documents->queryAll();
 
         return $this->render('index', ['documents' => $result, 'emailUser' => $emailUser],);
     }
-
-    public function actionDocument()
-    {
-        $query = "SELECT M_Revisi.NamaDoc
-                        FROM M_Revisi
-                        JOIN M_Document ON M_Revisi.IdDoc = M_Document.IdDoc
-                        WHERE M_Document.DocumentStatus = '5'";
-        $documents = Yii::$app->db->createCommand($query);
-        $result = $documents->query();
-        return $this->render('document', ['documents' => $result]);
-    }
+//
+//    public function actionDocument()
+//    {
+//        $query = "SELECT M_Revisi.NamaDoc
+//                        FROM M_Revisi
+//                        JOIN M_Document ON M_Revisi.IdDoc = M_Document.IdDoc
+//                        WHERE M_Document.DocumentStatus = '5'";
+//        $documents = Yii::$app->db->createCommand($query);
+//        $result = $documents->query();
+//        return $this->render('document', ['documents' => $result]);
+//    }
 }
